51d9a0d68031e321455c8862bc95064adead3dd7
katefeldy <150926686+katefeldy@users.noreply.github.com>
2023-11-16 02:35:19 +1100

    Add files via upload
    Basic draft of intro and verse completed, trialing various ways to incorporate chords.

---
+++ final verse and intro.rb
@@ -0,0 +1,117 @@
+use_synth :beep
+play :g, release:0.5
+sleep 0.5
+play :g, release:0.5
+sleep 0.5
+play :g, release:0.5
+sleep 0.5
+play :bb4, release:2
+use_synth :dark_ambience
+play chord(:Ab, :major), amp: 10, release: 1
+sleep 1
+use_synth :beep
+play :g, release: 2
+use_synth :dark_ambience
+play chord(:Bb, :major), amp: 10, release: 1
+sleep 1
+use_synth :dark_ambience
+play chord(:C, :minor7), amp: 10, release: 1
+sleep 0.5
+
+use_synth :beep
+play :g, release:0.5
+sleep 0.5
+use_synth :beep
+use_bpm 60
+play :g, release:0.5
+sleep 0.5
+play :g, release:0.5
+sleep 0.5
+play :bb4, release:2
+use_synth :dark_ambience
+play chord(:Ab, :major), amp: 10, release: 1
+use_synth :beep
+sleep 1
+play :g, release: 0.5
+use_synth :dark_ambience
+play chord(:Bb, :major), amp: 10, release: 1
+sleep 0.5
+use_synth :beep
+play :g, release:0.5
+sleep 0.5
+play :c, release:1
+use_synth :dark_ambience
+play chord(:C, :minor7), amp: 10, release: 1
+sleep 1
+use_synth :beep
+play :g, release:0.5
+sleep 0.5
+play :g, release:0.5
+sleep 0.5
+play :ab, release:0.5
+sleep 0.5
+play :bb, release:1
+use_synth :dark_ambience
+play chord(:bb, :major), release: 3, amp:10
+use_synth :beep
+sleep 1
+play :ab, release:0.5
+sleep 0.5
+play :g, release:0.5
+sleep 0.5
+play :f, release:0.5
+sleep 0.5
+play :eb, release:0.5
+use_synth :dark_ambience
+play chord(:ab, :major), release: 3, amp:10
+use_synth :beep
+sleep 0.5
+play :c, release:0.5
+sleep 0.5
+play :eb, release:0.5
+sleep 0.5
+play :eb, release:1
+sleep 1
+play :f, release:0.5
+sleep 0.5
+play :eb, release:1
+use_synth :dark_ambience
+play chord(:bb, :major), release: 3, amp:10
+use_synth :beep
+sleep 1
+use_synth :beep
+play :Bb, release: 1
+use_synth :dark_ambience
+play chord(:bb, :major), release: 3, amp:10
+sleep 1
+use_synth :beep
+play :G, release: 1
+use_synth :dark_ambience
+play chord(:bb, :major), release: 3, amp:10
+sleep 1
+use_synth :beep
+play :eb, release: 1
+sleep 1
+play :f, release: 1
+use_synth :dark_ambience
+play chord(:f, :minor), release: 3, amp:10
+sleep 3
+use_synth :beep
+play :Bb, release: 1
+use_synth :dark_ambience
+play chord(:bb, :major), release: 3, amp:10
+sleep 1
+use_synth :beep
+play :G, release: 1
+use_synth :dark_ambience
+play chord(:bb, :major), release: 3, amp:10
+sleep 1
+use_synth :beep
+play :eb, release: 1
+sleep 1
+play :f, release: 1
+use_synth :dark_ambience
+play chord(:f, :minor), release: 3, amp:10
+sleep 1
+use_synth :beep
+play :eb, release: 1
