4b5c28a8e8805ead0db9ab65c55a1ebf07145cbd
katefeldy <150926686+katefeldy@users.noreply.github.com>
2023-11-16 01:49:34 +1100

    Add files via upload
    Attempting to add drum layers. Encountered issues with playing simultaneous layers using loops, couldn't figure out how to start loops at specific times instead of manually typing beats - tedious process. Trying to make it sound lofi.

---
+++ First drum layers.rb
@@ -0,0 +1,88 @@
+# Welcome to Sonic Pi
+
+
+play :Bb, release: 2
+sleep 1
+play :G, release: 1
+sleep 0.5
+play :C, release: 2
+sleep 0.5
+sample :drum_bass_soft
+use_synth :dark_ambience
+play chord(:ab, :major), release: 3, amp:10
+sleep 1
+sample :drum_bass_soft
+sleep 1
+use_synth :beep
+play :Bb, release: 2
+sleep 1
+play :G, release: 1
+sleep 0.5
+play :C, release: 2
+sleep 0.5
+sample :drum_bass_soft
+use_synth :dark_ambience
+play chord(:bb, :major), release: 3, amp:10
+sleep 1
+sample :drum_bass_soft
+sleep 1
+use_synth :beep
+play :Bb, release: 2
+sleep 1
+play :G, release: 1
+sleep 0.5
+play :Bb3, release: 2
+sleep 0.5
+sample :drum_bass_soft
+use_synth :dark_ambience
+play chord(:c, :major), release: 3, amp:10
+use_synth :beep
+sleep 0.5
+play :c, release: 2
+sleep 0.5
+sample :drum_bass_soft
+sleep 1
+sample :drum_bass_soft
+sleep 0.4
+play :Bb3, release: 1
+sleep 0.5
+play :C, release: 1
+sleep 0.5
+play :Eb, release: 1
+sleep 0.5
+use_synth :dark_ambience
+play chord(:ab, :major), release: 3, amp:10
+use_synth :beep
+sleep 0.5
+use_synth :dark_ambience
+play chord(:ab, :major), release: 3, amp:10
+use_synth :beep
+sleep 2
+play :Bb, release: 1
+sleep 0.5
+play :C, release: 2
+sleep 0.5
+use_synth :dark_ambience
+play chord(:ab, :major), release: 3, amp:10
+use_synth :beep
+sleep 2
+play :Bb, release: 1
+sleep 0.5
+play :C, release: 2
+sleep 0.5
+use_synth :dark_ambience
+play chord(:bb, :major), release: 3, amp:10
+use_synth :beep
+sleep 2
+play :g, release: 2
+sleep 1
+play :f, release: 2
+use_synth :dark_ambience
+play chord(:ab, :major), release: 3, amp: 10
+use_synth :beep
+sleep 1
+play :eb, release: 1
+sleep 0.5
+play :c, release: 1
+
+
