5b6d96b7c2ba4e668f8a3f4bb01d1b435f846f98
katefeldy <150926686+katefeldy@users.noreply.github.com>
2023-11-16 01:39:29 +1100

    Add files via upload
    First completed attempt at song intro - began song with just beep like sounds, lacked creativity. Tedious process of getting tempo as close as possible to original song. Beginning to slow song down and deciding on 60bpm. Tried various bpm but as a beginner, layering became easier with slower tempo. Foundational learning of main melody without complexities.

---
+++ Intro 1.rb
@@ -0,0 +1,52 @@
+# Welcome to Sonic Pi
+play :c, release: 6
+play chord( :Ab, :major), release: 4, amp: 0.4
+use_synth :fm
+sleep 4
+sleep 2
+# Welcome to Sonic Pi
+
+play :Bb, release: 2
+sleep 1
+play :G, release: 1
+sleep 0.5
+play :C, release: 2
+sleep 0.3
+sleep 2
+play :Bb, release: 2
+sleep 1
+play :G, release: 1
+sleep 0.5
+play :C, release: 2
+sleep 0.3
+sleep 2
+play :Bb, release: 2
+sleep 1
+play :G, release: 1
+sleep 0.5
+play :Bb3, release: 2
+sleep 0.3
+play :c, release: 1
+sleep 0.3
+sleep 2
+play :Bb3, release: 1
+sleep 0.6
+play :C, release: 1
+sleep 0.5
+play :Eb, release: 1
+sleep 2
+play :Bb, release: 1
+sleep 0.5
+play :C, release: 1
+sleep 1
+play :Bb, release: 1
+sleep 2
+play :C, release: 1
+sleep 0.
+  play :g, release: 2
+sleep 1
+play :f, release: 2
+sleep 1
+play :eb, release: 1
+sleep 0.5
+play :c, release: 1
