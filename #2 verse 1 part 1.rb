1732c48171b4370954902274ba7f883c9615f70d
katefeldy <150926686+katefeldy@users.noreply.github.com>
2023-11-16 02:26:18 +1100

    Add files via upload
    First part of the verse edited to be slower. Adding second layer. Removed reverb to fix sound issue.

---
+++ #2 verse 1 part 1.rb
@@ -0,0 +1,44 @@
+use_synth :beep
+play :g, release:0.5
+sleep 0.5
+play :g, release:0.5
+sleep 0.5
+play :g, release:0.5
+sleep 0.5
+play :bb4, release:2
+use_synth :dark_ambience
+play chord(:Ab, :major), amp: 10, release: 1
+sleep 1
+use_synth :beep
+play :g, release: 2
+use_synth :dark_ambience
+play chord(:Bb, :major), amp: 10, release: 1
+sleep 1
+use_synth :dark_ambience
+play chord(:C, :minor7), amp: 10, release: 1
+sleep 0.5
+use_synth :beep
+play :g, release:0.5
+sleep 0.5
+use_synth :beep
+use_bpm 60
+play :g, release:0.5
+sleep 0.5
+play :g, release:0.5
+sleep 0.5
+play :bb4, release:2
+use_synth :dark_ambience
+play chord(:Ab, :major), amp: 10, release: 1
+use_synth :beep
+sleep 1
+play :g, release: 0.5
+use_synth :dark_ambience
+play chord(:Bb, :major), amp: 10, release: 1
+sleep 0.5
+use_synth :beep
+play :g, release:0.5
+sleep 0.5
+play :c, release:1
+use_synth :dark_ambience
+play chord(:C, :minor7), amp: 10, release: 1
+sleep 1
