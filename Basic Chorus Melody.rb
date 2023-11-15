16395420296dcdec46b9d025a0743d58d58de7f7
katefeldy <150926686+katefeldy@users.noreply.github.com>
2023-11-16 02:35:54 +1100

    Add files via upload
    Basic Chorus Melody completed

---
+++ Basic Chorus Melody.rb
@@ -0,0 +1,101 @@
+use_synth :beep
+play :eb, release: 0.5
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :G, release: 2
+sleep 2
+
+play :eb, release: 0.5
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :d, release: 2
+sleep 2
+
+play :C, release: 0.5
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :g, release: 1
+sleep 1
+play :bb, release: 1
+sleep 1
+play :c, release: 1
+sleep 1
+play :g, release: 1
+sleep 1
+
+
+play :eb, release: 0.5
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :g, release: 1
+sleep 2
+
+play :eb, release: 0.5
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :d, release: 1
+sleep 2
+
+
+play :eb, release: 0.5
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :g, release: 1
+sleep 1
+play :bb, release: 2
+sleep 2
+
+play :bb, release: 0.5
+sleep 0.5
+play :bb, release: 1
+sleep 0.5
+play :bb, release: 1
+sleep 1
+play :G, release:2
+sleep 2
+
+play :bb, release: 1
+sleep 0.5
+play :g, release: 0.5
+sleep 0.5
+play :f, release: 2
+sleep 2
+
+play :bb, release: 1
+sleep 1
+play :g, release: 1
+sleep 0.5
+play :bb3, release: 0.5
+sleep 0.5
+play :c, release: 1
+sleep 1
