31633283dc346eac6059b88e8802b8becda71c94
katefeldy <150926686+katefeldy@users.noreply.github.com>
2023-11-16 01:29:46 +1100

    Add files via upload
    Early stages - Discovering the use of chords and how these can form a layer of the song

---
+++ Remix ideas.rb
@@ -0,0 +1,12 @@
+
+    use_synth :dark_ambience
+    play chord(:ab, :major), release: 4, amp:10
+    sleep 1
+    use_synth :dark_ambience
+    play chord(:bb, :major), release: 4, amp:10
+    sleep 1
+    use_synth :dark_ambience
+    play chord(:c, :major), release: 4, amp:10
+    sleep 1
+  end
+end
