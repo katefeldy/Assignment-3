5a4407ea469b46a3cb173e818f7b64be7897027e
katefeldy <150926686+katefeldy@users.noreply.github.com>
2023-11-16 01:45:13 +1100

    Add files via upload
    Finally decided on synth choices - 'beep' and 'dark_ambience.' Tedious process of getting tempo right, minor tempo consistencies remain. Beginning to research more coding techniques ie. 'attack' to mimic dramatic intro. Incorporating A2 feedback (lacking orginality and creativity), decided to attempt a Lofi version of the song which matches capabilities of Sonic Pi and slow tempo. Beginning to layer chords and notes at specific times in the piece and gaining understanding of looping. Initially wanting to repeat intro twice using '2.times do'

---
+++ First complete intro.rb
@@ -0,0 +1,112 @@
+# Welcome to Sonic Pi
+
+
+use_synth :dark_ambience
+play :c, amp: 8, release: 6, attack: 4
+play chord(:Ab, :major), release: 4, amp: 1, attack: 4
+2.times do
+  use_synth :beep
+  sleep 7
+  play :Bb, release: 2
+  sleep 1
+  play :G, release: 1
+  sleep 0.5
+  play :C, release: 2
+  sleep 0.5
+  use_synth :dark_ambience
+  play chord(:ab, :major), release: 3, amp:10
+  use_synth :beep
+  sleep 2
+  play :Bb, release: 2
+  sleep 1
+  play :G, release: 1
+  sleep 0.5
+  play :C, release: 2
+  sleep 0.5
+  use_synth :dark_ambience
+  play chord(:bb, :major), release: 3, amp:10
+  sleep 2
+  use_synth :beep
+  play :Bb, release: 2
+  sleep 1
+  play :G, release: 1
+  sleep 0.5
+  play :Bb3, release: 2
+  sleep 0.5
+  use_synth :dark_ambience
+  play chord(:c, :major), release: 3, amp:10
+  use_synth :beep
+  sleep 0.3
+  play :c, release: 1
+  sleep 0.3
+  sleep 2
+  play :Bb3, release: 1
+  sleep 0.6
+  play :C, release: 1
+  sleep 0.5
+  play :Eb, release: 1
+  sleep 0.5
+  use_synth :dark_ambience
+  play chord(:ab, :major), release: 3, amp:10
+  use_synth :beep
+  sleep 2
+  play :Bb, release: 2
+  sleep 1
+  play :G, release: 1
+  sleep 0.5
+  play :C, release: 2
+  sleep 0.5
+  use_synth :dark_ambience
+  play chord(:bb, :major), release: 3, amp:10
+  sleep 2
+  use_synth :beep
+  play :Bb, release: 2
+  sleep 1
+  play :G, release: 1
+  sleep 0.5
+  play :Bb3, release: 2
+  sleep 0.5
+  use_synth :dark_ambience
+  play chord(:c, :major), release: 3, amp:10
+  use_synth :beep
+  sleep 0.3
+  play :c, release: 1
+  sleep 0.3
+  sleep 2
+  play :Bb3, release: 1
+  sleep 0.6
+  play :C, release: 1
+  sleep 0.5
+  play :Eb, release: 1
+  sleep 0.5
+  use_synth :dark_ambience
+  play chord(:ab, :major), release: 3, amp:10
+  use_synth :beep
+  sleep 2
+  play :Bb, release: 1
+  sleep 0.5
+  play :C, release: 2
+  sleep 0.5
+  use_synth :dark_ambience
+  play chord(:ab, :major), release: 3, amp:10
+  use_synth :beep
+  sleep 2
+  play :Bb, release: 1
+  sleep 0.5
+  play :C, release: 2
+  sleep 0.5
+  use_synth :dark_ambience
+  play chord(:bb, :major), release: 3, amp:10
+  use_synth :beep
+  sleep 2
+  play :g, release: 2
+  sleep 1
+  play :f, release: 2
+  use_synth :dark_ambience
+  play chord(:ab, :major), release: 3, amp: 10
+  use_synth :beep
+  sleep 1
+  play :eb, release: 1
+  sleep 0.5
+  play :c, release: 1
+end
